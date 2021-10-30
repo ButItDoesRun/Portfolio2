996d2a52cbb44b7f07195dd8337ad0113375fb07
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-10-30 19:18:19 +0200

    AdjacencyListGraph implemented -Adjacenylist class -Vertex class -Edge class -static method insertGraphData (insert data to make graph)

---
+++ src/com/company/AdjacencyListGraph.java
@@ -0,0 +1,149 @@
+package com.company;
+import java.util.ArrayList;
+
+
+//Use an adjacency vertex graph to make a graph with the data provided on the next pages.
+// (Hint have a Vertex prev=null and Integer dist= Integer.MAX_VALUE on the vertex)
+public class AdjacencyListGraph {
+    private ArrayList<Vertex> vertices; //list of vertices
+
+
+    public AdjacencyListGraph(){
+        vertices = new ArrayList<Vertex>();
+    }
+
+
+    //Function for making a vertex
+    public void addVertex(Vertex v){
+        vertices.add(v);
+    }
+
+    //Function for making a edge
+    public void newEdge(Vertex from, Vertex to, Integer dist){
+        if(!vertices.contains(from) && vertices.contains(to)){
+            System.out.println("Vertex not found");
+            return;
+
+        }
+
+        Edge newE = new Edge(from, to, dist);
+
+    }
+
+    //A function that prints the graph
+    public void printGraph(){
+        Vertex currentV;
+        for(int i = 0; i < vertices.size(); i++){
+            currentV = vertices.get(i);
+            System.out.println("Edges from Vertex: " + currentV.getName());
+            for(int j = 0; j < currentV.getOutEdges().size(); j++){
+                Edge currentE = currentV.getOutEdges().get(j);
+                System.out.println("To " + currentE.getToV().getName() + " weight: " + currentE.getWeight());
+            }
+            System.out.println(" ");
+
+        }
+    }
+
+
+}
+
+
+class Vertex implements Comparable<Vertex>{
+    private String name;
+    private ArrayList<Edge> outEdges;
+    Integer distance = Integer.MAX_VALUE;
+
+    public Vertex(String id){
+        this.name = id;
+        outEdges = new ArrayList<>();
+    }
+
+    //function for adding existing edges (the ones made with newEdge()) to graph
+    public void addOutEdges(Edge outEdge){
+        outEdges.add(outEdge);
+    }
+
+    @Override
+    public int compareTo(Vertex o) {
+        if(this.distance < o.distance){
+            return -1;
+        }
+
+        if(this.distance > o.distance){
+            return 1;
+        }
+
+        return 0;
+
+    }
+
+    //All getters and setters for class (generated) --> right-click, generate, all options clicked.
+    //(this is needed because variable this.name is private
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Edge> getOutEdges() {
+        return outEdges;
+    }
+
+    public void setOutEdges(ArrayList<Edge> outEdges) {
+        this.outEdges = outEdges;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+}
+
+
+
+
+class Edge{
+    private Vertex fromV;
+    private Vertex toV;
+    private Integer weight; // change to Integer to Double if too big
+
+    public Edge(Vertex from, Vertex to, Integer cost){ //cost is weight
+        fromV = from;
+        toV = to;
+        weight = cost;
+        from.addOutEdges(this); //method to make it easier to add edges (less text)
+    }
+
+
+    //Getter and setter methods (generated)
+    public Vertex getFromV() {
+        return fromV;
+    }
+
+    public void setFromV(Vertex fromV) {
+        this.fromV = fromV;
+    }
+
+    public Vertex getToV() {
+        return toV;
+    }
+
+    public void setToV(Vertex toV) {
+        this.toV = toV;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,128 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        AdjacencyListGraph adjGraph = insertGraphData();
+
+        adjGraph.printGraph();
+
+
+
+    }
+
+
+
+    //Creating the Adjacency vertex graph with data from the assigment datatable
+    public static AdjacencyListGraph insertGraphData(){
+        //Making an arraylist of vertices
+        AdjacencyListGraph adjG = new AdjacencyListGraph();
+
+        //adding all vertices
+        //The vertex is added outside to it can be accessed when using edges
+        Vertex Esk = new Vertex("Eskildstrup");
+        adjG.addVertex(Esk);
+
+        Vertex Has = new Vertex("Haslev");
+        adjG.addVertex(Has);
+
+        Vertex Hol = new Vertex("Holbæk");
+        adjG.addVertex(Hol);
+
+        Vertex Jær = new Vertex("Jærgerspris");
+        adjG.addVertex(Jær);
+
+        Vertex Kal = new Vertex("Kalundborg");
+        adjG.addVertex(Kal);
+
+        Vertex Kor = new Vertex("Korsør");
+        adjG.addVertex(Kor);
+
+        Vertex Køg = new Vertex("Køge");
+        adjG.addVertex(Køg);
+
+        Vertex Mar = new Vertex("Maribo");
+        adjG.addVertex(Mar);
+
+        Vertex Næs = new Vertex("Næstved");
+        adjG.addVertex(Næs);
+
+        Vertex Rin = new Vertex("Ringsted");
+        adjG.addVertex(Rin);
+
+        Vertex Sla = new Vertex("Slagelse");
+        adjG.addVertex(Sla);
+
+        Vertex Nyk = new Vertex("Nykøbing F");
+        adjG.addVertex(Nyk);
+
+        Vertex Vor = new Vertex("Vordingborg");
+        adjG.addVertex(Vor);
+
+        Vertex Ros = new Vertex("Roskilde");
+        adjG.addVertex(Ros);
+
+        Vertex Sor = new Vertex("Sorø");
+        adjG.addVertex(Sor);
+
+        Vertex Nak = new Vertex("Nakskov");
+        adjG.addVertex(Nak);
+
+
+        //Adding the Edges
+        //A.addOutEdges(new Edge(A, B, 1));
+        adjG.newEdge(Esk, Mar, 28);
+        adjG.newEdge(Esk, Nyk, 13);
+        adjG.newEdge(Esk, Vor, 25);
+        adjG.newEdge(Has, Kor, 60);
+        adjG.newEdge(Has, Køg, 24);
+        adjG.newEdge(Has, Næs, 25);
+        adjG.newEdge(Has, Rin, 19);
+        adjG.newEdge(Has, Ros, 47);
+        adjG.newEdge(Has, Sla, 48);
+        adjG.newEdge(Has, Sor, 34);
+        adjG.newEdge(Has, Vor, 40);
+        adjG.newEdge(Hol, Jær, 34);
+        adjG.newEdge(Hol, Kal, 44);
+        adjG.newEdge(Hol, Kor, 66);
+        adjG.newEdge(Hol, Rin, 36);
+        adjG.newEdge(Hol, Ros, 32);
+        adjG.newEdge(Hol, Sla, 46);
+        adjG.newEdge(Hol, Sor, 34);
+        adjG.newEdge(Jær, Kor, 94);
+        adjG.newEdge(Jær, Køg, 58);
+        adjG.newEdge(Jær, Rin, 56);
+        adjG.newEdge(Jær, Ros, 33);
+        adjG.newEdge(Jær, Sla, 74);
+        adjG.newEdge(Jær, Sor, 63);
+        adjG.newEdge(Kal, Rin, 62);
+        adjG.newEdge(Kal, Ros, 70);
+        adjG.newEdge(Kal, Sla, 39);
+        adjG.newEdge(Kal, Sor, 51);
+        adjG.newEdge(Kor, Næs, 45);
+        adjG.newEdge(Kor, Sla, 20);
+        adjG.newEdge(Køg, Næs, 45);
+        adjG.newEdge(Køg, Rin, 28);
+        adjG.newEdge(Køg, Ros, 25);
+        adjG.newEdge(Køg, Vor, 60);
+        adjG.newEdge(Mar, Nak, 27);
+        adjG.newEdge(Mar, Nyk, 26);
+        adjG.newEdge(Næs, Ros, 57);
+        adjG.newEdge(Næs, Rin, 26);
+        adjG.newEdge(Næs, Sla, 37);
+        adjG.newEdge(Næs, Sor, 32);
+        adjG.newEdge(Næs, Vor, 28);
+        adjG.newEdge(Rin, Ros, 31);
+        adjG.newEdge(Rin, Sor, 15);
+        adjG.newEdge(Rin, Vor, 58);
+        adjG.newEdge(Sla, Sor, 14);
+
+
+
+
+
+
+
+
+        return adjG;
+
     }
 }
