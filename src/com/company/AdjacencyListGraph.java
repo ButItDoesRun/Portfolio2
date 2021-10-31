0813ca728a6301b0e3a0fa7d0f0e2cb2bbf33c6d
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-10-31 04:53:17 +0100

    Unused code-sections deleted - comments rectified / shortened - faulty code is deleted

--- src/com/company/AdjacencyListGraph.java
+++ src/com/company/AdjacencyListGraph.java
@@ -1,9 +1,6 @@
 package com.company;
 import java.util.ArrayList;
 
-
-//Use an adjacency vertex graph to make a graph with the data provided on the next pages.
-// (Hint have a Vertex prev=null and Integer dist= Integer.MAX_VALUE on the vertex)
 public class AdjacencyListGraph {
     private ArrayList<Vertex> vertices; //list of vertices
 
@@ -47,37 +44,25 @@ public class AdjacencyListGraph {
     }
 
 
-    //method for inserting vertices into a heap
-    public MinHeap<Vertex> addAdjGraphToHeap(){
-        MinHeap<Vertex> newHeap = new MinHeap<>();
-
-        for(int i = 0; i < vertices.size(); i++){
-            newHeap.insert(vertices.get(i));
-        }
-
-        return newHeap;
-    }
-
     public void MSTPrims() {
         MinHeap<Vertex> Q = new MinHeap<>();
-        //To make a spannning tree you need to know the distances between to vertices, and where a node cam from
+        //To make a spannning tree you need to know the distances between to vertices, and where a node came from
         //Because of the adj-datastructure my elements need to be accessed through a iteration of the list
         for (int vertex = 0; vertex < this.vertices.size(); vertex++) {
             //all vertex dist are set to Max_value (infinity) - basically says its infinitely far from the other nodes
             vertices.get(vertex).setDistance(Integer.MAX_VALUE);
             //since we don't start out with a predecessor before we iterate we set them all to null
             vertices.get(vertex).setPrevious(null); //can put -1 instead
-            vertices.get(vertex).visited = false;
             Q.insert(vertices.get(vertex));
         }
 
-        //as we don't wanna set distance to the root if its empty.
+        //Only if the root isn't empty can we set a distance
         if (vertices.size() > 0) {
             int root = 0;
             vertices.get(root).setDistance(0); //if not empty distance to the roo is 0 - meaning we are starting at 0.
             int position = Q.getPosition(vertices.get(root)); //getting the position of the starting vertex
 
-            Q.decreaseKey(position); //Fixing the minheap since we changed the value of the starting vertex.
+            Q.decreaseKey(position); //Fixing minheap when root value changes
             int MST = 0;
 
             while (!Q.isEmpty()) {
@@ -89,7 +74,7 @@ public class AdjacencyListGraph {
                         v.getToV().setDistance(v.getWeight());
                         v.getToV().setPrevious(u);
                         int pos = Q.getPosition(v.getToV()); //get position
-                        Q.decreaseKey(pos);
+                        Q.decreaseKey(pos); //Fixing minheap when position changes
                     }
                 }
 
@@ -105,7 +90,7 @@ public class AdjacencyListGraph {
 
             System.out.println("Minimum Spanning Tree Total:");
             //Printing the total distance of MST
-            System.out.println("MST distance is: " + MST + " km.");
+            System.out.println("MST distance is: " + MST + " Km");
             //Printing the total price of the spanning tree
             System.out.println("MST price is: " + MST * 100000 + "DKK");
 
@@ -115,37 +100,6 @@ public class AdjacencyListGraph {
 
         }
     }
-
-
-    /*
-    public void reverseAdjGraph(){
-        Vertex currentFrom;
-        Vertex newFrom;
-        AdjacencyListGraph reverseGraph = new AdjacencyListGraph();
-        ArrayList<Edge> currentFromOutEdgesList;
-        ArrayList<Vertex> adjList = vertices;
-
-
-        for(int i = 0; i < adjList.size(); i++){
-            currentFrom = adjList.get(i);
-            currentFromOutEdgesList = currentFrom.getOutEdges();
-
-            for(int j = 0; j < currentFromOutEdgesList.size(); j++){
-                newFrom = adjList.get(j);
-
-
-                //add reverse vertex
-                reverseGraph.newEdge(newFrom, currentFrom, 1);
-
-            }
-        }
-    }
-
-     */
-
-
-
-
 }
 
 
@@ -154,7 +108,6 @@ class Vertex implements Comparable<Vertex>{
     private ArrayList<Edge> outEdges;
     Integer distance = Integer.MAX_VALUE;
     Vertex previous = null;
-    boolean visited = false;
 
     public Vertex(String id){
         this.name = id;
@@ -180,23 +133,13 @@ class Vertex implements Comparable<Vertex>{
 
     }
 
-    //All getters and setters for class (generated) --> right-click, generate, all options clicked.
-    //(this is needed because variable this.name is private
-
+    //Getters and Setters for class, because variables is rivate
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public ArrayList<Edge> getOutEdges() {
         return outEdges;
-    }
-
-    public void setOutEdges(ArrayList<Edge> outEdges) {
-        this.outEdges = outEdges;
     }
 
     public Integer getDistance() {
@@ -207,21 +150,10 @@ class Vertex implements Comparable<Vertex>{
         this.distance = distance;
     }
 
-    public Vertex getPrevious() {
-        return previous;
-    }
-
     public void setPrevious(Vertex previous) {
         this.previous = previous;
     }
 
-    public boolean isVisited() {
-        return visited;
-    }
-
-    public void setVisited(boolean visited) {
-        this.visited = visited;
-    }
 }
 
 
@@ -240,30 +172,15 @@ class Edge {
     }
 
 
-    //Getter and setter methods (generated)
-    public Vertex getFromV() {
-        return fromV;
-    }
-
-    public void setFromV(Vertex fromV) {
-        this.fromV = fromV;
-    }
-
+    //Getter and setter for class
     public Vertex getToV() {
         return toV;
-    }
-
-    public void setToV(Vertex toV) {
-        this.toV = toV;
     }
 
     public Integer getWeight() {
         return weight;
     }
 
-    public void setWeight(Integer weight) {
-        this.weight = weight;
-    }
   }
 
 
